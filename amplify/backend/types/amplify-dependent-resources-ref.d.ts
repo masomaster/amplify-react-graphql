14fe9c532bc315f7c8cac9e28e66d3b6515c3f47
mlancast <mlancast@pointloma.edu>
2024-04-12 12:36:44 -0700

    adds graphql api and storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyreactgraphqldd45d5d7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "imagestore": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,13 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://lbogmsb77rhr3p4n3sre7riooq.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-szkkujxw7bb7zkhsc7qpzzg72a",
+    "aws_user_files_s3_bucket": "amplifyreactgraphql05038daf886d4dbfb2df67d2f010f1522-staging",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
